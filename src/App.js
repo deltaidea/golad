e550544f5b941daf379ad9cfeee25c91dbf3c2f0
Nikita Litvin <n@deltaidea.com>
2017-03-09 05:40:10 +0500

    feat(BackgroundPaper): apply dark background color to body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import BackgroundPaper from './BackgroundPaper';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const App = () => (
-  <MuiThemeProvider>
-    <RaisedButton label="Hello world" />
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <BackgroundPaper>
+      <RaisedButton label="Hello world" />
+    </BackgroundPaper>
   </MuiThemeProvider>
 );
 
