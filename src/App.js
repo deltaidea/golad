da7b918d26664b86ab29ce50c1e0350d77dc0960
Nikita Litvin <n@deltaidea.com>
2017-03-09 06:20:36 +0500

    feat(App): add a card with 1v1 invite button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardText} from 'material-ui/Card';
 
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     <BackgroundPaper>
-      <RaisedButton label="Hello world" />
+      <div style={{textAlign: "center"}}>
+        <Card>
+          <CardText>
+            <RaisedButton label="1v1" />
+            <p>A regular 1v1 game. Press to get a link. Share the link with the opponent. The game will start once they open it.</p>
+          </CardText>
+        </Card>
+      </div>
     </BackgroundPaper>
   </MuiThemeProvider>
 );
