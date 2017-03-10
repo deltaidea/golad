c5319ea203bbcf5395050689d5a6fdc3d7c8c823
Nikita Litvin <n@deltaidea.com>
2017-03-10 11:42:16 +0500

    feat(HomePage): make random game mode first on mobile & center on large

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -3,25 +3,29 @@ import GameModeCard from './GameModeCard';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import GameTitleCard from './GameTitleCard';
 
-const SameSizeCol = ({children}) => <Col xs={12} md={8} lg={4}>{children}</Col>
-
-const GameModeCol = (props) => <SameSizeCol><GameModeCard {...props} /></SameSizeCol>
+const colSizing = {xs: 12, md: 8, lg: 4}
 
 const HomePage = () => (
   <Grid fluid>
     <Row center="xs">
-      <SameSizeCol><GameTitleCard /></SameSizeCol>
+      <Col {...colSizing}><GameTitleCard /></Col>
     </Row>
     <Row center="xs">
-      <GameModeCol
-        name="Play with a friend"
-        description="Press to get a link. Share the link with the opponent. The game will start once they open it." />
-      <GameModeCol
-        name="Local game"
-        description="Play with a friend on the same device. Take turns." />
-      <GameModeCol
-        name="Random opponent"
-        description="We will choose an opponent for you among others currently on the website." />
+      <Col {...colSizing}>
+        <GameModeCard
+          name="Random opponent"
+          description="We will choose an opponent for you among others currently on the website." />
+      </Col>
+      <Col {...colSizing} first="lg">
+        <GameModeCard
+          name="Play with a friend"
+          description="Press to get a link. Share the link with the opponent. The game will start once they open it." />
+      </Col>
+      <Col {...colSizing}>
+        <GameModeCard
+          name="Local game"
+          description="Play with a friend on the same device. Take turns." />
+      </Col>
     </Row>
   </Grid>
 );
