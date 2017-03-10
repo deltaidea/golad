b3b349b57968fb5f3229f24ce1e083f2b754b1bc
Nikita Litvin <n@deltaidea.com>
2017-03-10 11:11:24 +0500

    refactor(HomePage): deduplicate props by abstracting to common component

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -3,32 +3,25 @@ import GameModeCard from './GameModeCard';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import GameTitleCard from './GameTitleCard';
 
+const SameSizeCol = ({children}) => <Col xs={12} md={8} lg={4}>{children}</Col>
+
+const GameModeCol = (props) => <SameSizeCol><GameModeCard {...props} /></SameSizeCol>
+
 const HomePage = () => (
   <Grid fluid>
     <Row center="xs">
-      <Col xs={12} md={8} lg={4}>
-        <GameTitleCard />
-      </Col>
+      <SameSizeCol><GameTitleCard /></SameSizeCol>
     </Row>
-    <Row center="xs" style={{textAlign: "center"}}>
-      <Col xs={12} md={8} lg={4} style={{textAlign: "center"}}>
-        <GameModeCard
-          name="Play with a friend"
-          description="Press to get a link. Share the link with the opponent. The game will start once they open it."
-        ></GameModeCard>
-      </Col>
-      <Col xs={12} md={8} lg={4} style={{textAlign: "center"}}>
-        <GameModeCard
-          name="Local game"
-          description="Play with a friend on the same device. Take turns."
-        ></GameModeCard>
-      </Col>
-      <Col xs={12} md={8} lg={4} style={{textAlign: "center"}}>
-        <GameModeCard
-          name="Random opponent"
-          description="We will choose an opponent for you among others currently on the website."
-        ></GameModeCard>
-      </Col>
+    <Row center="xs">
+      <GameModeCol
+        name="Play with a friend"
+        description="Press to get a link. Share the link with the opponent. The game will start once they open it." />
+      <GameModeCol
+        name="Local game"
+        description="Play with a friend on the same device. Take turns." />
+      <GameModeCol
+        name="Random opponent"
+        description="We will choose an opponent for you among others currently on the website." />
     </Row>
   </Grid>
 );
