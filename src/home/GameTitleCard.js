26e4e418ebc87136a9df905340160f06021283d3
Nikita Litvin <n@deltaidea.com>
2017-03-10 10:44:13 +0500

    feat(Icon): accept square colors (player and opponent) as props

--- src/home/GameTitleCard.js
+++ src/home/GameTitleCard.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import {Card, CardText} from 'material-ui/Card';
+import {red500, indigo500} from 'material-ui/styles/colors';
 import Icon from './Icon';
 
 const GameModeCard = () => (
-  <Card style={{
-    marginTop: '30px',
-  }}>
+  <Card style={{marginTop: '30px'}}>
     <CardText>
-      <Icon style={{
-        display: 'inline-block',
-        marginBottom: '3em',
-        width: '50%',
-        maxWidth: '300px',
-      }}/>
-      <div style={{
-        fontSize: '2em',
-      }}>Game of Life and Death</div>
+      <Icon width='50%' maxWidth='300px' playerColor={red500} opponentColor={indigo500}/>
+      <div style={{marginTop: '1em', fontSize: '2em'}}>Game of Life and Death</div>
     </CardText>
   </Card>
 );
--- src/home/Icon.js
+++ src/home/Icon.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Icon = ({style}) => (
-  <svg viewBox="0 0 270 170" style={style} xmlns="http://www.w3.org/2000/svg">
-    <rect height="70" width="70" y="0" x="0" fill="#e53935"/>
-    <rect height="70" width="70" y="0" x="100" fill="#e53935"/>
-    <rect height="70" width="70" y="100" x="100" fill="#3949ab"/>
-    <path d="M 200 100 h 70 v 50 l -20 20 h -50 Z" fill="#3949ab"/>
+const Icon = ({width, maxWidth, playerColor, opponentColor}) => (
+  <svg viewBox="0 0 270 170" width={width} style={{maxWidth}} xmlns="http://www.w3.org/2000/svg">
+    <rect height="70" width="70" y="0" x="0" fill={playerColor}/>
+    <rect height="70" width="70" y="0" x="100" fill={playerColor}/>
+    <rect height="70" width="70" y="100" x="100" fill={opponentColor}/>
+    <path d="M 200 100 h 70 v 50 l -20 20 h -50 Z" fill={opponentColor}/>
   </svg>
 );
 
