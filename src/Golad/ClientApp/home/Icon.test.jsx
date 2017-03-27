6ef6fd157829275bcc58027568c922915bc90859
Nikita Litvin <n@deltaidea.com>
2017-03-27 21:12:18 +0500

    test(Icon): check that `width` and `max-width` are applied

--- src/Golad/ClientApp/home/Icon.test.jsx
+++ src/Golad/ClientApp/home/Icon.test.jsx
@@ -8,3 +8,10 @@ test('it renders svg with specified colors', () => {
   expect(icon.find('rect[x="0"][y="0"]').prop('fill')).toEqual('#111111');
   expect(icon.find('rect[x="100"][y="100"]').prop('fill')).toEqual('#222222');
 });
+
+test('it sets specified width and max-width', () => {
+  const icon = shallow(<Icon width='30%' maxWidth={500} />);
+
+  expect(icon.find('svg').prop('width')).toEqual('30%');
+  expect(icon.find('svg').prop('style').maxWidth).toEqual(500);
+});
