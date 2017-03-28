1e179e204f2a4be3d10f74a26e52868a3f7468ee
Dmitry Svechnikov <d.svechnikov@gmail.com>
2017-03-28 17:11:05 +0500

    feat(backend-tests): add example tests: one passing and one failing

---
+++ tests/Golad.Tests/ExampleTests.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace Golad.Tests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var a = 1;
+            var b = 1;
+
+            var result = a + b;
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            var a = 1;
+            var b = 1;
+
+            var result = a + b;
+
+            Assert.Equal(3, result);
+        }
+    }
+}
