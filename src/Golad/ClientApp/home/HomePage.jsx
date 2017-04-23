27bf3d1a8358656d935cd91a063d76f578b6758e
Nikita Litvin <n@deltaidea.com>
2017-04-23 18:28:42 +0500

    feat(HomePage): add icons for game modes

--- src/Golad/ClientApp/home/HomePage.jsx
+++ src/Golad/ClientApp/home/HomePage.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {List, ListItem} from 'material-ui/List';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import Icon from './Icon';
+import IconRandomOpponent from 'material-ui/svg-icons/social/public';
+import IconPlayOnline from 'material-ui/svg-icons/social/people';
+import IconSingleplayer from 'material-ui/svg-icons/social/person';
+import IconPlayOnThisDevice from 'material-ui/svg-icons/hardware/smartphone';
 
 const HomePageHero = (props) => (<div style={{
   backgroundColor: '#202020',
@@ -27,10 +31,10 @@ class HomePage extends Component {
           <div style={{paddingTop: 20}}>Game of Life and Death</div>
         </HomePageHero>
         <List style={{padding: 0}}>
-          <ListItem primaryText="Random opponent" />
-          <ListItem primaryText="Singleplayer" />
-          <ListItem primaryText="Play with a friend" />
-          <ListItem primaryText="Local game" />
+          <ListItem leftIcon={<IconRandomOpponent />} primaryText="Random opponent" />
+          <ListItem leftIcon={<IconPlayOnline />} primaryText="Play with a friend online" />
+          <ListItem leftIcon={<IconSingleplayer />} primaryText="Singleplayer" />
+          <ListItem leftIcon={<IconPlayOnThisDevice />} primaryText="Play with a friend on this device" />
         </List>
       </div>
     )
